250ad3dcdc5630214556f05a2366cd76db2c4f5e
Whale77777 <kavyasathyanathan@gmail.com>
2023-08-08 11:15:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
